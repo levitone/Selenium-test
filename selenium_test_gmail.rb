b325867231edbf1ac8b6e54f4bada97685cc1052
levitone <levitone@brandeis.edu>
2017-11-10 17:53:39 -0500

    Add files via upload

---
+++ selenium_test_gmail.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'test-unit'
+
+class GmailLoginTest < Test::Unit::TestCase
+
+	def setup
+		@driver = Selenium::WebDriver.for :firefox
+		@driver.get "http://www.gmail.com"
+	end
+
+	def test_page_title
+		assert_equal @driver.title, "Gmail: Email from Google"
+	end
+
+	def test_login
+		loginBox = @driver.find_element(:id, "Email")
+		loginBox.send_keys("email.address@gmail.com")
+		pwBox = @driver.find_element(:id, "Passwd")
+		pwBox.send_keys("!SuperSecretpassw0rd")
+		signinBtn = @driver.find_element(:id, "signIn")
+		signinBtn.click()
+	end
+
+	def teardown
+		@driver.quit
+	end
+end
